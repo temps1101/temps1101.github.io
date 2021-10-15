3e9685d84ba52196f12f7d089afa5facc6193ddc
temps1101 <tsurutaitsuki11000@gmail.com>
2021-10-15 13:40:10 +0900

    ckpt

--- static/js/historic-timeline.js
+++ static/js/historic-timeline.js
@@ -1,4 +1,4 @@
 function startHistoricGame() {
-    var startPage = $('<div id="game-bg"><div id="start-page"><h1>RULE</h1><span>縄文、弥生、</span></div></div>')
+    var startPage = $('<div id="game-bg"><div id="start-page"><h1>RULE</h1><span>縄文、弥生、飛鳥、…などの年代ごとにわけて出題します。出来事のパネルを並び替えて回答してくださいねー！</span></div></div>')
     $('body').append(startPage);
 }
