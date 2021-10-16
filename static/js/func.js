cdb0cc518cb426473c000a41ff0e6e7c2d9bf29a
temps1101 <tsurutaitsuki11000@gmail.com>
2021-10-16 16:57:24 +0900

    [update?] 現実逃避だよ

--- static/js/func.js
+++ static/js/func.js
@@ -1,8 +1,8 @@
-function copyStringToClipboard () {
+function copyStringToClipboard() {
     var el = document.createElement('textarea');
     el.value = "temps1101#6781";
     el.setAttribute('readonly', '');
-    el.style = {position: 'absolute', left: '-9999px'};
+    el.style = { position: 'absolute', left: '-9999px' };
     document.body.appendChild(el);
     el.select();
     document.execCommand('copy'); // what?? why depricated!!!?????
@@ -10,15 +10,9 @@ function copyStringToClipboard () {
     alert('IDクリップボードに書いたよ！alertで知らせちゃってごめんちゃいっ！')
 }
 
-function sleep(waitMsec) {
-    var startMsec = new Date();
-    while (new Date() - startMsec < waitMsec);
-  }
-
-$(window).on('load', function() {
-    sleep(1000);
+$(window).on('load', function () {
     var $widget = $('iframe.twitter-timeline');
     var $widgetContents = $widget.contents();
     $widgetContents.find('head').append('<link rel="stylesheet" href="static/css/twitter.css">');
     $widgetContents.find('head').append('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@500&display=swap" rel="stylesheet">');
-  });
+});
