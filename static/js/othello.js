311b3e39504010eb52ccee9936ba75aefa8dfb97
temps1101 <62169032+temps1101@users.noreply.github.com>
2022-01-24 21:25:54 +0900

    一旦全削除

--- static/js/othello.js
+++
@@ -1,7 +0,0 @@
-fetch('https://raw.githubusercontent.com/temps1101/temps1101/main/README.md').then(function(response) {
-    response.text().then(function(text) {
-        board = text.match('<!--board-->.+<!--board-->')
-        alert(board)
-        $('#chessborad').text()
-    });
-});
